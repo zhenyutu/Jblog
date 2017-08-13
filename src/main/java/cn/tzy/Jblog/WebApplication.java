d9255dc158aad2c3f71569d5caea69125dfa422a
zhenyutu <1572629866@qq.com>
2017-08-13 17:00:27 +0800

    初始化工程结构

---
+++ src/main/java/cn/tzy/Jblog/WebApplication.java
@@ -0,0 +1,15 @@
+package cn.tzy.Jblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tuzhenyu on 17-8-13.
+ * @author tuzhenyu
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
---
+++ src/main/java/cn/tzy/Jblog/controller/IndexController.java
@@ -0,0 +1,18 @@
+package cn.tzy.Jblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by tuzhenyu on 17-8-13.
+ * @author tuzhenyu
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(path = {"/","/index"})
+    @ResponseBody
+    public String index(){
+        return "index";
+    }
+}
