17a89c9d8f4a9390bb11b78b29fe9d215f2aef3b
zhenyutu <1572629866@qq.com>
2017-08-14 17:04:24 +0800

    添加发布文章页面

--- src/main/resources/static/js/style.js
+++ src/main/resources/static/js/style.js
@@ -25,8 +25,17 @@ $(function () {
     });
   });
 
-  $('#tag').tokenfield()
+    $('#tag').tokenfield()
 
 
+    editormd("test-editormd", {
+        width   : "90%",
+        height  : 640,
+        syncScrolling : "single",
+        //你的lib目录的路径，我这边用JSP做测试的
+        path    : "<%=request.getContextPath()%>/resources/editormd/lib/",
+        //这个配置在simple.html中并没有，但是为了能够提交表单，使用这个配置可以让构造出来的HTML代码直接在第二个隐藏的textarea域中，方便post提交表单。
+        saveHTMLToTextarea : true
+    });
 
 });
